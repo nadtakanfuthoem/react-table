36a654fb03489bde190153ec402e4782f011c613
filmfuthoem <nadtakan.jones@gmail.com>
2020-02-14 13:44:52 -0800

    create util folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import { createTable } from '../src/helper';
+import { createTable } from '../src/util/helper';
 import './App.css';
 
 function App() {
